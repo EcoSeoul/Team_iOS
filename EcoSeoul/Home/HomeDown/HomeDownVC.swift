fda9a465c402c50a1fe69b3727dfa8b0ffaca403
choyeaeun <silbia629@gmail.com>
2018-09-15 01:47:34 +0900

    simple tableview code

--- EcoSeoul/Home/HomeDown/HomeDownVC.swift
+++ EcoSeoul/Home/HomeDown/HomeDownVC.swift
@@ -9,10 +9,12 @@
 import UIKit
 
 class HomeDownVC: UIViewController {
+    
 
     @IBOutlet weak var barcodeView: UILabel!
     
-
+    @IBOutlet weak var Tableview: UITableView!
+    
     @IBOutlet weak var barcodeBtn: UIButton!
     
     
@@ -20,7 +22,10 @@ class HomeDownVC: UIViewController {
         super.viewDidLoad()
         
         let image = generateBarcodeFromString(string: "lcs921125")
-        barcodeBtn.setBackgroundImage(image, for: .normal)
+        barcodeBtn.setImage(image, for: .normal)
+        
+        self.Tableview.dataSource = self
+        self.Tableview.delegate = self
 
     }
     
@@ -50,4 +55,55 @@ class HomeDownVC: UIViewController {
     }
     
     
+}
+
+extension HomeDownVC: UITableViewDataSource, UITableViewDelegate{
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return 2
+        }else {
+            return 4
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        if indexPath.section == 0 {
+            
+            let cell = Tableview.dequeueReusableCell(withIdentifier: "PointTVC") as! PointTVC
+            cell.categoryLB.text = "포인트 전환/삭제"
+            cell.titleLB.text = "TOP쇼핑"
+            cell.explainLB.text = "*에코머니 포인트로 결제하실 때는 결제화면에서 에코머니 비밀번호를 입력하셔야 합니다."
+            
+            
+            return cell
+        }else {
+            let cell = Tableview.dequeueReusableCell(withIdentifier: "MenuTVC") as! MenuTVC
+            cell.titleLB.text = "가맹점 찾기"
+            cell.explainLB.text = "내 주변 가맹점 및 할인 공공시설을 찾아보세요"
+            return cell
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let label = UILabel()
+        label.text = "에코머니 가맹점 온라인 몰 둘러보기"
+        //label.textColor.cgColor = "#707070"
+        if section == 0 {
+            return label
+        }else {
+            label.isHidden = true
+            return label
+        }
+        
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
 }
---
+++ EcoSeoul/Home/HomeDown/PointTVC.swift
@@ -0,0 +1,22 @@
+//
+//  PointTVC.swift
+//  EcoSeoul
+//
+//  Created by 조예은 on 2018. 9. 15..
+//  Copyright © 2018년 GGOMMI. All rights reserved.
+//
+
+import UIKit
+
+class PointTVC: UITableViewCell {
+
+    @IBOutlet weak var categoryLB: UILabel!
+    @IBOutlet weak var titleLB: UILabel!
+    @IBOutlet weak var explainLB: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
