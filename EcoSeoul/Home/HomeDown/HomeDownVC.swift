8916ebb97a7089597e42a017bd88cc297ef4486a
choyeaeun <silbia629@gmail.com>
2018-09-17 22:38:40 +0900

    new tableview basic code

--- EcoSeoul/Home/HomeDown/HomeDownVC.swift
+++ EcoSeoul/Home/HomeDown/HomeDownVC.swift
@@ -119,6 +119,41 @@ extension HomeDownVC: UITableViewDataSource, UITableViewDelegate{
         
     }
 
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+//        self.Tableview.beginUpdates()
+        
+//        let indexSet = NSMutableIndexSet()
+//        indexSet.add(indexPath.section + 1)
+//        indexSet.add(indexPath.section + 2)
+        
+//        if indexPath.section == 0 {
+//            var expandCol : Bool = true
+//            if expandCol == true {
+//
+//                self.Tableview.deleteSections(NSIndexSet(index: 1) as IndexSet, with: UITableViewRowAnimation.automatic)
+//                self.Tableview.deleteSections(NSIndexSet(index: 2) as IndexSet, with: UITableViewRowAnimation.automatic)
+//                expandCol = false
+//
+//            }else {
+//                self.Tableview.insertSections(NSIndexSet(index: 1) as IndexSet, with: UITableViewRowAnimation.automatic)
+//                self.Tableview.insertSections(NSIndexSet(index: 2) as IndexSet, with: UITableViewRowAnimation.automatic)
+//                expandCol = true
+//            }
+//
+//        }
+//        self.Tableview.endUpdates()
+        if indexPath.section == 0{
+            let button = UIButton(type: .system)
+            button.setTitle("ggggggggg", for: .normal)
+            button.addTarget(self, action: #selector(handleExpandClose), for: .touchUpInside)
+        }
+    }
+    
+    @objc func handleExpandClose(){
+        print("ggg")
+    }
+
     ////////////////header 관련////////////////
     
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
