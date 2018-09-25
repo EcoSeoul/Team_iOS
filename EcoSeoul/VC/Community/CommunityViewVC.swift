a7a2cfce704c12d069ae53c6481ee0af491f5107
choyeaeun <silbia629@gmail.com>
2018-09-25 21:11:23 +0900

    community comment bar shadow + mypage current tab

--- EcoSeoul/VC/Community/CommunityViewVC.swift
+++ EcoSeoul/VC/Community/CommunityViewVC.swift
@@ -12,6 +12,8 @@ class CommunityViewVC: UIViewController, UITableViewDataSource, UITableViewDeleg
     
 
     @IBOutlet weak var tableview: UITableView!
+    @IBOutlet weak var commentSendView: UIView!
+    var keyboardDismissGesture: UITapGestureRecognizer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +23,7 @@ class CommunityViewVC: UIViewController, UITableViewDataSource, UITableViewDeleg
         
         self.tableview.tableFooterView = UIView(frame: .zero)
         setNaviBar()
+        commnentBarShadow()
     }
     
     var backBtn: UIBarButtonItem = {
@@ -39,7 +42,16 @@ class CommunityViewVC: UIViewController, UITableViewDataSource, UITableViewDeleg
     }
     
     
-    
+    @IBOutlet weak var commentBar: UIView!
+
+    func commnentBarShadow(){
+        self.commentBar.layer.shadowColor = UIColor.black.cgColor
+        commentBar.layer.shadowOpacity = 0.3
+        commentBar.layer.shadowOffset = CGSize.zero
+        commentBar.layer.shadowRadius = 5
+        self.view.addSubview(commentBar)
+    }
+
     
     func setNaviBar(){
         backBtn.target = self
@@ -53,6 +65,8 @@ class CommunityViewVC: UIViewController, UITableViewDataSource, UITableViewDeleg
         bar.backgroundColor = .white
         item.title = ""
         bar.shadowImage = UIImage()
+        
+        //print("네비바의 높이:" + bar.)
         
     }
     
@@ -75,5 +89,53 @@ class CommunityViewVC: UIViewController, UITableViewDataSource, UITableViewDeleg
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 70
+    }
+}
+
+
+extension CommunityViewVC{
+    func setKeyboardSetting() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: .UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: .UIKeyboardWillHide, object: nil)
+    }
+    
+    @objc func keyboardWillShow(_ notification: Notification) {
+        adjustKeyboardDismissGesture(isKeyboardVisible: true)
+        
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            //////// 키보드의 사이즈만큼 commentSendView의 y축을 위로 이동시킴 ////////
+            commentSendView.frame.origin.y -= keyboardSize.height
+            ////////
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    @objc func keyboardWillHide(_ notification: Notification) {
+        adjustKeyboardDismissGesture(isKeyboardVisible: false)
+        
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            //////// 키보드의 사이즈만큼 commentSendView의 y축을 아래로 이동시킴 ////////
+            commentSendView.frame.origin.y += keyboardSize.height
+            ////////
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    func adjustKeyboardDismissGesture(isKeyboardVisible: Bool) {
+        if isKeyboardVisible {
+            if keyboardDismissGesture == nil {
+                keyboardDismissGesture = UITapGestureRecognizer(target: self, action: #selector(tapBackground))
+                view.addGestureRecognizer(keyboardDismissGesture!)
+            }
+        } else {
+            if keyboardDismissGesture != nil {
+                view.removeGestureRecognizer(keyboardDismissGesture!)
+                keyboardDismissGesture = nil
+            }
+        }
+    }
+    
+    @objc func tapBackground() {
+        self.view.endEditing(true)
     }
 }
--- EcoSeoul/VC/Pay/PayTabBar/PayVC.swift
+++ EcoSeoul/VC/Pay/PayTabBar/PayVC.swift
@@ -13,10 +13,16 @@ class PayVC: ButtonBarPagerTabStripViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        loadViewDesign()
         configureButtonBar()
+        
     }
-    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        let check = UserDefaults.standard.object(forKey: "check") as! Int
+        if check == 1 {
+            moveToViewController(at: 1)
+        }
+    }
     override func viewControllers(for pagerTabStripController: PagerTabStripViewController) -> [UIViewController]{
         let child1 = UIStoryboard(name: "Pay", bundle: nil).instantiateViewController(withIdentifier: "MileageTVC")
         let child2 = UIStoryboard(name: "Pay", bundle: nil).instantiateViewController(withIdentifier: "MoneyTVC")
@@ -26,64 +32,7 @@ class PayVC: ButtonBarPagerTabStripViewController {
     @IBAction func dismissBtn(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
-    
-//    func loadViewDesign(){
-//        self.settings.style.selectedBarHeight = 1
-//        self.settings.style.buttonBarBackgroundColor = UIColor.white
-//        self.settings.style.buttonBarItemBackgroundColor = UIColor.white
-//        self.settings.style.selectedBarBackgroundColor = .white
-//        self.settings.style.selectedBarVerticalAlignment = .bottom
-//        self.settings.style.buttonBarMinimumLineSpacing = 0
-//        self.settings.style.buttonBarItemTitleColor = #colorLiteral(red: 0.4392156863, green: 0.4392156863, blue: 0.4392156863, alpha: 1)
-//        self.settings.style.buttonBarItemFont = UIFont(name: notoSansFont.Medium.rawValue, size: 17)!
-//        self.settings.style.buttonBarItemsShouldFillAvailableWidth = true
-//        self.settings.style.buttonBarLeftContentInset = 10
-//        self.settings.style.buttonBarRightContentInset = 10
-//        self.settings.style.buttonBarHeight = 44
-//        let buttonBar = ButtonBarView(frame: CGRect(x: 0, y: 60, width: view.frame.size.width, height: self.settings.style.buttonBarHeight!), collectionViewLayout: UICollectionViewFlowLayout())
-//        buttonBar.backgroundColor = .white
-//        buttonBar.selectedBar.backgroundColor = .white
-//        buttonBar.autoresizingMask = .flexibleWidth
-//        var newContainerViewFrame = containerView.frame
-//        newContainerViewFrame.origin.y = 104
-//        newContainerViewFrame.size.height = containerView.frame.size.height - (self.settings.style.buttonBarHeight! - containerView.frame.origin.y)
-//        containerView.frame = newContainerViewFrame
-//
-//        self.settings.style.selectedBarBackgroundColor = UIColor.white
-////        let buttonBarViewAux = buttonBarView ?? {
-////            let flowLayout = UICollectionViewFlowLayout()
-////            flowLayout.scrollDirection = .horizontal
-////            let buttonBarHeight = settings.style.buttonBarHeight ?? 44
-////            let buttonBar = ButtonBarView(frame: CGRect(x: 0, y: 60, width: view.frame.size.width, height: buttonBarHeight), collectionViewLayout: flowLayout)
-////            buttonBar.backgroundColor = .white
-////            buttonBar.selectedBar.backgroundColor = .white
-////            buttonBar.autoresizingMask = .flexibleWidth
-////            var newContainerViewFrame = containerView.frame
-////            newContainerViewFrame.origin.y = 104
-////            newContainerViewFrame.size.height = containerView.frame.size.height - (buttonBarHeight - containerView.frame.origin.y)
-////            containerView.frame = newContainerViewFrame
-////            return buttonBar
-////            }()
-////        buttonBarView = buttonBarViewAux
-////
-////        if buttonBarView.superview == nil {
-////            view.addSubview(buttonBarView)
-////        }
-////        if buttonBarView.delegate == nil {
-////            buttonBarView.delegate = self
-////        }
-////        if buttonBarView.dataSource == nil {
-////            buttonBarView.dataSource = self
-////        }
-////        buttonBarView.scrollsToTop = false
-//
-//        changeCurrentIndexProgressive = { (oldCell: ButtonBarViewCell, newCell: ButtonBarViewCell, progressPercentage: CGFloat, changeCurrentIndex: Bool, animated: Bool) -> Void in
-//            guard changeCurrentIndex == true else { return }
-//            oldCell.label.textColor = UIColor.white
-//            newCell.label.textColor = UIColor.white
-//
-//            } as? ((ButtonBarViewCell?, ButtonBarViewCell?, CGFloat, Bool, Bool) -> Void)
-//    }
+
     
     func configureButtonBar() {
         // Sets the background colour of the pager strip and the pager strip item
@@ -91,7 +40,7 @@ class PayVC: ButtonBarPagerTabStripViewController {
         settings.style.buttonBarItemBackgroundColor = .white
         
         // Sets the pager strip item font and font color
-        settings.style.buttonBarItemFont = UIFont(name: "Helvetica", size: 16.0)!
+        settings.style.buttonBarItemFont = UIFont(name: "NotoSansCJKkr-Medium", size: 16.0)!
         settings.style.buttonBarItemTitleColor = .gray
         
         // Sets the pager strip item offsets
