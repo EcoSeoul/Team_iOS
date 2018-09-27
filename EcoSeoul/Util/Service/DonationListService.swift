5826d82f906ab39dc637526e0053ebecb4a948e3
GGOMMI <lcs3303@gmail.com>
2018-09-28 04:14:33 +0900

    add Service struct (Request, Donation, Post)

--- EcoSeoul/Util/Service/DonationListService.swift
+++ EcoSeoul/Util/Service/DonationListService.swift
@@ -9,3 +9,35 @@
 import Foundation
 import Alamofire
 import SwiftyJSON
+
+struct DonationListService: GettableService {
+    
+    typealias NetworkData = DonationList
+    static let shareInstance = DonationListService()
+    
+    func getDonationData(url : String, completion : @escaping (NetworkResult<[DonationListData]>) -> Void){
+        
+        get(url) { (result) in
+            switch result {
+            case .success(let networkResult):
+                switch networkResult.message {
+                case "Successfully Get Organizations Data" :
+                    completion(.networkSuccess(networkResult.myDonations))
+                case "Internal Server Error!" :
+                    completion(.serverErr)
+                default :
+                    break
+                }
+                
+                break
+            case .error(let errMsg) :
+                print(errMsg)
+                break
+            case .failure(_) :
+                completion(.networkFail)
+            }
+        }
+        
+    }
+    
+}
--- EcoSeoul/Util/Service/PostListService.swift
+++ EcoSeoul/Util/Service/PostListService.swift
@@ -9,3 +9,35 @@
 import Foundation
 import Alamofire
 import SwiftyJSON
+
+struct PostListService: GettableService {
+    
+    typealias NetworkData = PostList
+    static let shareInstance = PostListService()
+    
+    func getPostData(url : String, completion : @escaping (NetworkResult<[PostListData]>) -> Void){
+        
+        get(url) { (result) in
+            switch result {
+            case .success(let networkResult):
+                switch networkResult.message {
+                case "ok" :
+                    completion(.networkSuccess(networkResult.myTextList))
+                case "Internal Server Error!" :
+                    completion(.serverErr)
+                default :
+                    break
+                }
+                
+                break
+            case .error(let errMsg) :
+                print(errMsg)
+                break
+            case .failure(_) :
+                completion(.networkFail)
+            }
+        }
+        
+    }
+    
+}
