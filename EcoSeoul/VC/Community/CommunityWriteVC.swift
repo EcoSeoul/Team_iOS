2c04ddbaaf5a79f30ed6fd29c0a38f9d7c6c8665
choyeaeun <silbia629@gmail.com>
2018-09-28 03:56:22 +0900

    community write network

--- EcoSeoul/VC/Community/CommunityWriteVC.swift
+++ EcoSeoul/VC/Community/CommunityWriteVC.swift
@@ -8,8 +8,12 @@
 
 import UIKit
 
-class CommunityWriteVC: UIViewController {
+class CommunityWriteVC: UIViewController, APIService {
 
+    @IBOutlet weak var titleLB: UITextField!
+    @IBOutlet weak var contentLB: UITextView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -34,12 +38,49 @@ class CommunityWriteVC: UIViewController {
     }()
     
     @objc func registerVC(){
-        navigationController?.popViewController(animated: true)
+    
+        if (titleLB.text?.isEmpty)!{
+            simpleAlert(title: "오류", message: "제목을 입력하여 주십시오.")
+        }else if (contentLB.text == "내용을 입력하세요(1000자 이내)"){
+            simpleAlert(title: "오류", message: "내용을 입력하여 주십시오.")
+        }else{
+            
+                let params : [String : Any] = [
+                    "board_title" : self.titleLB.text as Any,
+                    "user_idx" : UserDefaults.standard.string(forKey: "userIdx")!,
+                    "board_content" : self.contentLB.text
+                ]
+                
+                CommunityWrite.shareInstance.registerboard(url: self.url("/board"), params: params) { [weak self] (result) in
+                    guard let `self` = self else { return }
+                    switch result {
+                    case .networkSuccess(_):
+                        
+                        self.simpleAlertwithHandler(title: "등록하기", message: "게시글을 등록하시겠습니까?", okHandler: { (_) in self.navigationController?.popViewController(animated: false)
+                        })
+                    case .nullValue :
+                        self.simpleAlert(title: "오류", message: "텍스트 비어있음")
+                    case .networkFail :
+                        self.simpleAlert(title: "오류", message: "인터넷 연결상태를 확인해주세요")
+                    default :
+                        break
+                    }
+                    
+                }
+            
+        }
     }
     
     
     @objc func popSelf() {
-        navigationController?.popViewController(animated: true)
+        if (titleLB.text?.isEmpty == false)||(contentLB.text.isEmpty == false){
+            simpleAlertwithHandler(title: "작성 취소", message: "내용이 저장되지 않습니다. \n작성을 취소하시겠습니까?", okHandler: { (_) in
+                
+                self.navigationController?.popViewController(animated: true)
+                
+                })
+        }
+        
     }
     
     func setNaviBar(){
